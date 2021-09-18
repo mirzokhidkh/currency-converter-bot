d3ce15e7a9dc0281c435c7b0957be27e6294497d
mirzokhidkhasanov <mirzohidkhasanov12@gmail.com>
2021-09-18 23:39:14 +0500

    keyboards added

--- src/main/java/uz/mk/Main.java
+++ src/main/java/uz/mk/Main.java
@@ -14,7 +14,6 @@ import org.telegram.telegrambots.updatesreceivers.DefaultBotSession;
 
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Locale;
 
 public class Main extends TelegramLongPollingBot {
     public static void main(String[] args) {
@@ -34,12 +33,13 @@ public class Main extends TelegramLongPollingBot {
 
     @Override
     public String getBotToken() {
-        return "1782234473:AAEKoRbQbvvg8RbAC2M-qIitn35DNvvrhUY";
+        return "1782234473:AAGGmvXKqMckr1ykMxSaGVM88Er_C5d7evI";
     }
 
     @Override
     public void onUpdateReceived(Update update) {
         SendMessage sendMessage = new SendMessage();
+
         if (update.hasCallbackQuery()) {
             sendMessage.setChatId(String.valueOf(update.getCallbackQuery().getMessage().getChatId()));
             String callbackText = update.getCallbackQuery().getData();
@@ -51,60 +51,65 @@ public class Main extends TelegramLongPollingBot {
                 sendMessage.setText("I don't understand");
 
             }
-        }
-        String inputText = update.getMessage().getText();
-        sendMessage.setChatId(String.valueOf(update.getMessage().getChatId()));
+        }else if (update.hasMessage()){
+            String inputText = update.getMessage().getText();
+            sendMessage.setChatId(String.valueOf(update.getMessage().getChatId()));
 
-        if (inputText.equals("/start")) {
-            InlineKeyboardMarkup inlineKeyboardMarkup = new InlineKeyboardMarkup();
-            sendMessage.setReplyMarkup(inlineKeyboardMarkup);
-            List<List<InlineKeyboardButton>> rowList = new ArrayList<>();
+            if (inputText.equals("/start")) {
+                InlineKeyboardMarkup inlineKeyboardMarkup = new InlineKeyboardMarkup();
+                sendMessage.setReplyMarkup(inlineKeyboardMarkup);
 
-            List<InlineKeyboardButton> row1 = new ArrayList<>();
-            InlineKeyboardButton inlineBtn1 = new InlineKeyboardButton("What is your name");
-            inlineBtn1.setCallbackData("name");
-            InlineKeyboardButton inlineBtn2 = new InlineKeyboardButton("What is your surname");
-            inlineBtn2.setCallbackData("surname");
-            row1.add(inlineBtn1);
-            row1.add(inlineBtn2);
-            rowList.add(row1);
+                List<List<InlineKeyboardButton>> rowList = new ArrayList<>();
 
-            List<InlineKeyboardButton> row2 = new ArrayList<>();
-            InlineKeyboardButton inlineBtn21 = new InlineKeyboardButton("What");
-            inlineBtn21.setCallbackData("Hello");
-            row2.add(inlineBtn21);
-            rowList.add(row2);
-            inlineKeyboardMarkup.setKeyboard(rowList);
-        } else {
-            if (inputText.toLowerCase().equals("hello")) {
-                sendMessage.setText("How are you");
+                List<InlineKeyboardButton> row1 = new ArrayList<>();
+                InlineKeyboardButton btn1 = new InlineKeyboardButton("What is your name");
+                btn1.setCallbackData("name");
+                InlineKeyboardButton btn2 = new InlineKeyboardButton("What is your surname");
+                btn2.setCallbackData("surname");
+                row1.add(btn1);
+                row1.add(btn2);
+                rowList.add(row1);
+
+                List<InlineKeyboardButton> row2 = new ArrayList<>();
+                InlineKeyboardButton btn2_1 = new InlineKeyboardButton("What");
+                btn2_1.setCallbackData("Hello");
+                row2.add(btn2_1);
+                rowList.add(row2);
+                inlineKeyboardMarkup.setKeyboard(rowList);
+                sendMessage.setText("Welcome to our bot");
+
+
             } else {
-                sendMessage.setText("I don't understand");
+                if (inputText.toLowerCase().equals("hello")) {
+                    sendMessage.setText("How are you");
+                } else {
+                    sendMessage.setText("I don't understand");
 
+                }
+                ReplyKeyboardMarkup keyboardMarkup = new ReplyKeyboardMarkup();
+                sendMessage.setReplyMarkup(keyboardMarkup);
+                keyboardMarkup.setResizeKeyboard(true);
+                keyboardMarkup.setOneTimeKeyboard(true);
+                keyboardMarkup.setSelective(true);
+
+                List<KeyboardRow> keyboardRows = new ArrayList<>();
+
+                KeyboardRow row1 = new KeyboardRow();
+                KeyboardButton button1 = new KeyboardButton("hello");
+                KeyboardButton button2 = new KeyboardButton("bye");
+                row1.add(button1);
+                row1.add(button2);
+
+                KeyboardRow row2 = new KeyboardRow();
+                row2.add("and you ?");
+
+                keyboardRows.add(row1);
+                keyboardRows.add(row2);
+
+                keyboardMarkup.setKeyboard(keyboardRows);
             }
-            ReplyKeyboardMarkup keyboardMarkup = new ReplyKeyboardMarkup();
-            sendMessage.setReplyMarkup(keyboardMarkup);
-            keyboardMarkup.setResizeKeyboard(true);
-            keyboardMarkup.setOneTimeKeyboard(true);
-            keyboardMarkup.setSelective(true);
-
-            List<KeyboardRow> keyboardRows = new ArrayList<>();
-
-            KeyboardRow row1 = new KeyboardRow();
-            KeyboardButton button1 = new KeyboardButton("hello");
-            KeyboardButton button2 = new KeyboardButton("bye");
-            row1.add(button1);
-            row1.add(button2);
-
-            KeyboardRow row2 = new KeyboardRow();
-            row2.add("and you");
-
-            keyboardRows.add(row1);
-            keyboardRows.add(row2);
-
-            keyboardMarkup.setKeyboard(keyboardRows);
-
         }
+
 
 
         try {
