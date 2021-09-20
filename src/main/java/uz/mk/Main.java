bfd3cc592e0b5c848b5abd28602f5aa772abc18f
mirzokhidkhasanov <mirzohidkhasanov12@gmail.com>
2021-09-21 01:42:23 +0500

    completed convertAmount method

--- src/main/java/uz/mk/Main.java
+++ src/main/java/uz/mk/Main.java
@@ -1,16 +1,28 @@
 package uz.mk;
 
+import com.google.common.reflect.TypeToken;
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
 import org.telegram.telegrambots.bots.TelegramLongPollingBot;
 import org.telegram.telegrambots.meta.TelegramBotsApi;
 import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
 import org.telegram.telegrambots.meta.api.methods.updatingmessages.EditMessageText;
 import org.telegram.telegrambots.meta.api.objects.CallbackQuery;
+import org.telegram.telegrambots.meta.api.objects.Message;
 import org.telegram.telegrambots.meta.api.objects.Update;
 import org.telegram.telegrambots.meta.api.objects.replykeyboard.InlineKeyboardMarkup;
 import org.telegram.telegrambots.meta.api.objects.replykeyboard.buttons.InlineKeyboardButton;
 import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
 import org.telegram.telegrambots.updatesreceivers.DefaultBotSession;
+import uz.mk.model.Currency;
 
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.lang.reflect.Type;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.net.URLConnection;
+import java.text.DecimalFormat;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -27,6 +39,8 @@ public class Main extends TelegramLongPollingBot {
 
     }
 
+    private String toCurrency;
+    private String fromCurrency;
 
     @Override
     public String getBotUsername() {
@@ -40,7 +54,8 @@ public class Main extends TelegramLongPollingBot {
 
     @Override
     public void onUpdateReceived(Update update) {
-        SendMessage sendMessage = new SendMessage();
+        SendMessage toMessage = new SendMessage();
+        Message fromMessage = update.getMessage();
 
         if (update.hasCallbackQuery()) {
             EditMessageText editMessageText = new EditMessageText();
@@ -52,70 +67,116 @@ public class Main extends TelegramLongPollingBot {
 
             InlineKeyboardMarkup inlineKeyboardMarkup = new InlineKeyboardMarkup();
 
-            editMessageText.setReplyMarkup(inlineKeyboardMarkup);
             List<List<InlineKeyboardButton>> rowList = new ArrayList<>();
 
-            if (callbackData.equals("usd")) {
-                editMessageText.setText("Choose one of these:");
-                List<InlineKeyboardButton> row1 = new ArrayList<>();
-                InlineKeyboardButton btn1 = new InlineKeyboardButton(UZS_USD);
-                btn1.setCallbackData("uzs_usd");
-                InlineKeyboardButton btn2 = new InlineKeyboardButton(USD_UZS);
-                btn2.setCallbackData("usd_uzs");
-                row1.add(btn1);
-                row1.add(btn2);
-                rowList.add(row1);
-            } else if (callbackData.equals("rub")) {
-                editMessageText.setText("Choose one of these:");
-                List<InlineKeyboardButton> row1 = new ArrayList<>();
-                InlineKeyboardButton btn1 = new InlineKeyboardButton(UZS_RUB);
-                btn1.setCallbackData("uzs_rub");
-                InlineKeyboardButton btn2 = new InlineKeyboardButton(RUB_USZ);
-                btn2.setCallbackData("rub_uzs");
-                row1.add(btn1);
-                row1.add(btn2);
-                rowList.add(row1);
+            List<InlineKeyboardButton> row1 = new ArrayList<>();
+            InlineKeyboardButton btn1;
+            InlineKeyboardButton btn2;
+
+            switch (callbackData) {
+                case USD:
+                    editMessageText.setText("Choose one of these:");
+                    row1 = new ArrayList<>();
+                    btn1 = createInlineKeyboardButton(UZS_USD_WITH_FLAG, UZS_USD);
+                    btn2 = createInlineKeyboardButton(USD_UZS_WITH_FLAG, USD_UZS);
+                    row1.add(btn1);
+                    row1.add(btn2);
+                    rowList.add(row1);
+                    inlineKeyboardMarkup.setKeyboard(rowList);
+                    editMessageText.setReplyMarkup(inlineKeyboardMarkup);
+//                    toCurrency = callbackData;
+                    break;
+                case RUB:
+                    editMessageText.setText("Choose one of these:");
+                    row1 = new ArrayList<>();
+                    btn1 = createInlineKeyboardButton(UZS_RUB_WITH_FLAG, UZS_RUB);
+                    btn2 = createInlineKeyboardButton(RUB_USZ_WITH_FLAG, RUB_USZ);
+                    row1.add(btn1);
+                    row1.add(btn2);
+                    rowList.add(row1);
+                    inlineKeyboardMarkup.setKeyboard(rowList);
+                    editMessageText.setReplyMarkup(inlineKeyboardMarkup);
+//                    toCurrency = callbackData;
+                    break;
+                case UZS_USD:
+                    fromCurrency = UZS;
+                    toCurrency = USD;
+                    editMessageText.setText("Enter amount which is converting :");
+                    break;
+                case USD_UZS:
+                    fromCurrency = USD;
+                    toCurrency = UZS;
+                    editMessageText.setText("Enter amount which is converting :");
+                    break;
+                case UZS_RUB:
+                    fromCurrency = UZS;
+                    toCurrency = RUB;
+                    editMessageText.setText("Enter amount which is converting :");
+                    break;
+                case RUB_USZ:
+                    fromCurrency = RUB;
+                    toCurrency = UZS;
+                    editMessageText.setText("Enter amount which is converting :");
+                    break;
+
             }
 
-            inlineKeyboardMarkup.setKeyboard(rowList);
 
             try {
                 execute(editMessageText);
             } catch (TelegramApiException e) {
                 e.printStackTrace();
             }
-        } else if (update.hasMessage() && update.getMessage().hasText()) {
-            String inputText = update.getMessage().getText();
-            sendMessage.setChatId(String.valueOf(update.getMessage().getChatId()));
 
-            if (inputText.equals("/start")) {
-                sendMessage.setText("Welcome to our currency converter bot. " +
-                        "This bot will help you to convert Uzbek Som into another currency.\n" +
-                        "Choose one of on the given currency units/");
+        } else if (update.hasMessage() && fromMessage.hasText()) {
+            String inputText = fromMessage.getText();
+            Long chatId = fromMessage.getChatId();
 
-                InlineKeyboardMarkup inlineKeyboardMarkup = new InlineKeyboardMarkup();
-                sendMessage.setReplyMarkup(inlineKeyboardMarkup);
+            toMessage.setChatId(String.valueOf(chatId));
 
-                List<List<InlineKeyboardButton>> rowList = new ArrayList<>();
 
-                List<InlineKeyboardButton> row1 = new ArrayList<>();
-                InlineKeyboardButton btn1 = new InlineKeyboardButton(USD);
-                btn1.setCallbackData("usd");
-                InlineKeyboardButton btn2 = new InlineKeyboardButton(RUB);
-                btn2.setCallbackData("rub");
-                row1.add(btn1);
-                row1.add(btn2);
-                rowList.add(row1);
-                inlineKeyboardMarkup.setKeyboard(rowList);
-
+            if (inputText.matches("^[0-9]+$")) {
+                Double amount = Double.parseDouble(inputText);
+                String convertedAmount = convertAmount(amount, fromCurrency, toCurrency);
+                toMessage.setText(amount + " " + fromCurrency + " = " + convertedAmount + " " + toCurrency);
                 try {
-                    execute(sendMessage);
+                    execute(toMessage);
                 } catch (TelegramApiException e) {
                     e.printStackTrace();
                 }
 
             }
 
+
+            switch (inputText) {
+                case "/start":
+                    toMessage.setText("Welcome to our currency converter bot. " +
+                            "This bot will help you to convert Uzbek Som into another currency.\n" +
+                            "Choose one of on the given currency units.");
+
+                    InlineKeyboardMarkup inlineKeyboardMarkup = new InlineKeyboardMarkup();
+                    toMessage.setReplyMarkup(inlineKeyboardMarkup);
+
+                    List<List<InlineKeyboardButton>> rowList = new ArrayList<>();
+
+                    List<InlineKeyboardButton> row1 = new ArrayList<>();
+                    InlineKeyboardButton button1 = createInlineKeyboardButton(USD_WITH_FLAG, USD);
+                    InlineKeyboardButton button2 = createInlineKeyboardButton(RUB_WITH_FLAG, RUB);
+                    row1.add(button1);
+                    row1.add(button2);
+                    rowList.add(row1);
+                    inlineKeyboardMarkup.setKeyboard(rowList);
+
+                    try {
+                        execute(toMessage);
+                    } catch (TelegramApiException e) {
+                        e.printStackTrace();
+                    }
+                    break;
+
+                case "convert":
+                    break;
+            }
 
 
 //                ReplyKeyboardMarkup keyboardMarkup = new ReplyKeyboardMarkup();
@@ -144,9 +205,51 @@ public class Main extends TelegramLongPollingBot {
     }
 
 
-    private Double convertAmount() {
+    private String convertAmount(Double amount, String fromCcy, String toCcy) {
+        Gson gson = new GsonBuilder().setPrettyPrinting().create();
+        ArrayList<Currency> currencies = new ArrayList<>();
+        String convertedAmount = null;
 
-        return null;
+        try {
+            URL url = new URL("https://cbu.uz/uz/arkhiv-kursov-valyut/json/");
+            URLConnection urlConnection = url.openConnection();
+            InputStreamReader reader = new InputStreamReader(urlConnection.getInputStream());
+            Type type = new TypeToken<ArrayList<Currency>>() {
+            }.getType();
+            currencies = gson.fromJson(reader, type);
+        } catch (MalformedURLException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        String ccy;
+        if (fromCcy.equals(UZS)) {
+            ccy = toCcy;
+        } else {
+            ccy = fromCcy;
+        }
+
+        DecimalFormat decimalFormat = new DecimalFormat("##0.###");
+
+        for (Currency currency : currencies) {
+            if (currency.getCcy().equals(ccy)) {
+                if (fromCcy.equals(UZS)) {
+                    convertedAmount = decimalFormat.format(amount / Double.parseDouble(currency.getRate()));
+                } else {
+                    convertedAmount = String.valueOf(amount * Double.parseDouble(currency.getRate()));
+                }
+                break;
+            }
+        }
+        return convertedAmount;
+    }
+
+
+    public InlineKeyboardButton createInlineKeyboardButton(String text, String callbackData) {
+        InlineKeyboardButton inlineKeyboardButton = new InlineKeyboardButton(text);
+        inlineKeyboardButton.setCallbackData(callbackData);
+        return inlineKeyboardButton;
     }
 
 
--- src/main/java/uz/mk/utils/KeyboardButtons.java
+++ src/main/java/uz/mk/utils/KeyboardButtons.java
@@ -1,12 +1,19 @@
 package uz.mk.utils;
 
 public interface KeyboardButtons {
-    final String USD = "USD\uD83C\uDDFA\uD83C\uDDF8";
-    final String RUB = "RUB\uD83C\uDDF7\uD83C\uDDFA";
-    final String UZS_USD = "\uD83C\uDDFA\uD83C\uDDFFUZS \uD83D\uDD04 USD\uD83C\uDDFA\uD83C\uDDF8";
-    final String USD_UZS = "\uD83C\uDDFA\uD83C\uDDF8USD \uD83D\uDD04 UZS\uD83C\uDDFA\uD83C\uDDFF";
-    final String UZS_RUB = "\uD83C\uDDFA\uD83C\uDDFFUZS \uD83D\uDD04 RUB\uD83C\uDDF7\uD83C\uDDFA";
-    final String RUB_USZ = "\uD83C\uDDF7\uD83C\uDDFARUB \uD83D\uDD04 UZS\uD83C\uDDFA\uD83C\uDDFF";
+    final String UZS = "UZS";
+    final String USD = "USD";
+    final String RUB = "RUB";
+    final String USD_WITH_FLAG = "USD\uD83C\uDDFA\uD83C\uDDF8";
+    final String RUB_WITH_FLAG = "RUB\uD83C\uDDF7\uD83C\uDDFA";
+    final String UZS_USD = "UZS-USD";
+    final String USD_UZS = "USD-UZS";
+    final String UZS_RUB = "UZS-RUB";
+    final String RUB_USZ = "RUB-UZS";
+    final String UZS_USD_WITH_FLAG = "\uD83C\uDDFA\uD83C\uDDFFUZS \uD83D\uDD04 USD\uD83C\uDDFA\uD83C\uDDF8";
+    final String USD_UZS_WITH_FLAG = "\uD83C\uDDFA\uD83C\uDDF8USD \uD83D\uDD04 UZS\uD83C\uDDFA\uD83C\uDDFF";
+    final String UZS_RUB_WITH_FLAG = "\uD83C\uDDFA\uD83C\uDDFFUZS \uD83D\uDD04 RUB\uD83C\uDDF7\uD83C\uDDFA";
+    final String RUB_USZ_WITH_FLAG = "\uD83C\uDDF7\uD83C\uDDFARUB \uD83D\uDD04 UZS\uD83C\uDDFA\uD83C\uDDFF";
 
 
 }
