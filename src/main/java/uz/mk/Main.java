21c699ed6d9cf6cf9eecf7829734632e6dae3da8
mirzokhidkhasanov <mirzohidkhasanov12@gmail.com>
2021-09-20 23:31:18 +0500

    added some inline keyboards: USD, RUB

--- src/main/java/uz/mk/Main.java
+++ src/main/java/uz/mk/Main.java
@@ -3,17 +3,18 @@ package uz.mk;
 import org.telegram.telegrambots.bots.TelegramLongPollingBot;
 import org.telegram.telegrambots.meta.TelegramBotsApi;
 import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
+import org.telegram.telegrambots.meta.api.methods.updatingmessages.EditMessageText;
+import org.telegram.telegrambots.meta.api.objects.CallbackQuery;
 import org.telegram.telegrambots.meta.api.objects.Update;
 import org.telegram.telegrambots.meta.api.objects.replykeyboard.InlineKeyboardMarkup;
-import org.telegram.telegrambots.meta.api.objects.replykeyboard.ReplyKeyboardMarkup;
 import org.telegram.telegrambots.meta.api.objects.replykeyboard.buttons.InlineKeyboardButton;
-import org.telegram.telegrambots.meta.api.objects.replykeyboard.buttons.KeyboardButton;
-import org.telegram.telegrambots.meta.api.objects.replykeyboard.buttons.KeyboardRow;
 import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
 import org.telegram.telegrambots.updatesreceivers.DefaultBotSession;
 
 import java.util.ArrayList;
 import java.util.List;
+
+import static uz.mk.utils.KeyboardButtons.*;
 
 public class Main extends TelegramLongPollingBot {
     public static void main(String[] args) {
@@ -25,6 +26,7 @@ public class Main extends TelegramLongPollingBot {
         }
 
     }
+
 
     @Override
     public String getBotUsername() {
@@ -41,82 +43,111 @@ public class Main extends TelegramLongPollingBot {
         SendMessage sendMessage = new SendMessage();
 
         if (update.hasCallbackQuery()) {
-            sendMessage.setChatId(String.valueOf(update.getCallbackQuery().getMessage().getChatId()));
-            String callbackText = update.getCallbackQuery().getData();
-            if (callbackText.equals("name")) {
-                sendMessage.setText("John");
-            } else if (callbackText.equals("surname")) {
-                sendMessage.setText("Doe");
-            } else {
-                sendMessage.setText("I don't understand");
+            EditMessageText editMessageText = new EditMessageText();
+            CallbackQuery callbackQuery = update.getCallbackQuery();
+            String callbackData = update.getCallbackQuery().getData();
 
+            editMessageText.setChatId(String.valueOf(callbackQuery.getMessage().getChatId()));
+            editMessageText.setMessageId(callbackQuery.getMessage().getMessageId());
+
+            InlineKeyboardMarkup inlineKeyboardMarkup = new InlineKeyboardMarkup();
+
+            editMessageText.setReplyMarkup(inlineKeyboardMarkup);
+            List<List<InlineKeyboardButton>> rowList = new ArrayList<>();
+
+            if (callbackData.equals("usd")) {
+                editMessageText.setText("Choose one of these:");
+                List<InlineKeyboardButton> row1 = new ArrayList<>();
+                InlineKeyboardButton btn1 = new InlineKeyboardButton(UZS_USD);
+                btn1.setCallbackData("uzs_usd");
+                InlineKeyboardButton btn2 = new InlineKeyboardButton(USD_UZS);
+                btn2.setCallbackData("usd_uzs");
+                row1.add(btn1);
+                row1.add(btn2);
+                rowList.add(row1);
+            } else if (callbackData.equals("rub")) {
+                editMessageText.setText("Choose one of these:");
+                List<InlineKeyboardButton> row1 = new ArrayList<>();
+                InlineKeyboardButton btn1 = new InlineKeyboardButton(UZS_RUB);
+                btn1.setCallbackData("uzs_rub");
+                InlineKeyboardButton btn2 = new InlineKeyboardButton(RUB_USZ);
+                btn2.setCallbackData("rub_uzs");
+                row1.add(btn1);
+                row1.add(btn2);
+                rowList.add(row1);
             }
-        }else if (update.hasMessage()){
+
+            inlineKeyboardMarkup.setKeyboard(rowList);
+
+            try {
+                execute(editMessageText);
+            } catch (TelegramApiException e) {
+                e.printStackTrace();
+            }
+        } else if (update.hasMessage() && update.getMessage().hasText()) {
             String inputText = update.getMessage().getText();
             sendMessage.setChatId(String.valueOf(update.getMessage().getChatId()));
 
             if (inputText.equals("/start")) {
+                sendMessage.setText("Welcome to our currency converter bot. " +
+                        "This bot will help you to convert Uzbek Som into another currency.\n" +
+                        "Choose one of on the given currency units/");
+
                 InlineKeyboardMarkup inlineKeyboardMarkup = new InlineKeyboardMarkup();
                 sendMessage.setReplyMarkup(inlineKeyboardMarkup);
 
                 List<List<InlineKeyboardButton>> rowList = new ArrayList<>();
 
                 List<InlineKeyboardButton> row1 = new ArrayList<>();
-                InlineKeyboardButton btn1 = new InlineKeyboardButton("What is your name");
-                btn1.setCallbackData("name");
-                InlineKeyboardButton btn2 = new InlineKeyboardButton("What is your surname");
-                btn2.setCallbackData("surname");
+                InlineKeyboardButton btn1 = new InlineKeyboardButton(USD);
+                btn1.setCallbackData("usd");
+                InlineKeyboardButton btn2 = new InlineKeyboardButton(RUB);
+                btn2.setCallbackData("rub");
                 row1.add(btn1);
                 row1.add(btn2);
                 rowList.add(row1);
-
-                List<InlineKeyboardButton> row2 = new ArrayList<>();
-                InlineKeyboardButton btn2_1 = new InlineKeyboardButton("What");
-                btn2_1.setCallbackData("Hello");
-                row2.add(btn2_1);
-                rowList.add(row2);
                 inlineKeyboardMarkup.setKeyboard(rowList);
-                sendMessage.setText("Welcome to our bot");
 
-
-            } else {
-                if (inputText.toLowerCase().equals("hello")) {
-                    sendMessage.setText("How are you");
-                } else {
-                    sendMessage.setText("I don't understand");
-
+                try {
+                    execute(sendMessage);
+                } catch (TelegramApiException e) {
+                    e.printStackTrace();
                 }
-                ReplyKeyboardMarkup keyboardMarkup = new ReplyKeyboardMarkup();
-                sendMessage.setReplyMarkup(keyboardMarkup);
-                keyboardMarkup.setResizeKeyboard(true);
-                keyboardMarkup.setOneTimeKeyboard(true);
-                keyboardMarkup.setSelective(true);
 
-                List<KeyboardRow> keyboardRows = new ArrayList<>();
-
-                KeyboardRow row1 = new KeyboardRow();
-                KeyboardButton button1 = new KeyboardButton("hello");
-                KeyboardButton button2 = new KeyboardButton("bye");
-                row1.add(button1);
-                row1.add(button2);
-
-                KeyboardRow row2 = new KeyboardRow();
-                row2.add("and you ?");
-
-                keyboardRows.add(row1);
-                keyboardRows.add(row2);
-
-                keyboardMarkup.setKeyboard(keyboardRows);
             }
+
+
+
+//                ReplyKeyboardMarkup keyboardMarkup = new ReplyKeyboardMarkup();
+//                sendMessage.setReplyMarkup(keyboardMarkup);
+//                keyboardMarkup.setResizeKeyboard(true);
+//                keyboardMarkup.setOneTimeKeyboard(true);
+//                keyboardMarkup.setSelective(true);
+//
+//                List<KeyboardRow> keyboardRows = new ArrayList<>();
+//
+//                KeyboardRow row1 = new KeyboardRow();
+//                row1.add(new KeyboardButton(UZS_USD));
+//                row1.add(new KeyboardButton(USD_UZS));
+//
+//                KeyboardRow row2 = new KeyboardRow();
+//                row2.add(new KeyboardButton(UZS_RUB));
+//                row2.add(new KeyboardButton(RUB_USZ));
+//                keyboardRows.add(row1);
+//                keyboardRows.add(row2);
+//
+//                keyboardMarkup.setKeyboard(keyboardRows);
+
         }
 
-
-
-        try {
-            execute(sendMessage);
-        } catch (TelegramApiException e) {
-            e.printStackTrace();
-        }
 
     }
+
+
+    private Double convertAmount() {
+
+        return null;
+    }
+
+
 }
---
+++ src/main/java/uz/mk/utils/KeyboardButtons.java
@@ -0,0 +1,12 @@
+package uz.mk.utils;
+
+public interface KeyboardButtons {
+    final String USD = "USD\uD83C\uDDFA\uD83C\uDDF8";
+    final String RUB = "RUB\uD83C\uDDF7\uD83C\uDDFA";
+    final String UZS_USD = "\uD83C\uDDFA\uD83C\uDDFFUZS \uD83D\uDD04 USD\uD83C\uDDFA\uD83C\uDDF8";
+    final String USD_UZS = "\uD83C\uDDFA\uD83C\uDDF8USD \uD83D\uDD04 UZS\uD83C\uDDFA\uD83C\uDDFF";
+    final String UZS_RUB = "\uD83C\uDDFA\uD83C\uDDFFUZS \uD83D\uDD04 RUB\uD83C\uDDF7\uD83C\uDDFA";
+    final String RUB_USZ = "\uD83C\uDDF7\uD83C\uDDFARUB \uD83D\uDD04 UZS\uD83C\uDDFA\uD83C\uDDFF";
+
+
+}
